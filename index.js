5072e6ef4c2c15e7d78d20399594edef336b026d
vietvd13 <vuducviet0131@gmail.com>
2020-12-26 21:49:44 +0700

    Change PORT 5000

--- index.js
+++ index.js
@@ -10,17 +10,17 @@ const app = express();
 app.set('view engine', 'hbs');
 app.set('views', './views');
 hbs.registerPartials(__dirname + '/views/partials');
-// hbs.registerHelper('add', function (index) {
-//     index++;
-//     return index;
-// });
+hbs.registerHelper('add', function (index) {
+    index++;
+    return index;
+});
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/products', productRouter);
 
-const PORT = 8000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
 
